42eca1b0d74d3de29eca282226b52e8ee8cd9768
Mark Underseth <marku@s2technologies.com>
2026-07-11 08:11:58 -0700

    new input

--- tests/components/new-project.js
+++ tests/components/new-project.js
@@ -1,4 +1,7 @@
-async function newproject(page, { p1, p2 } ) {
+async function newproject(page, { p1, p2, m1 } ) {
+    p1 = "one";
+    p2 = "two";
+    m1 = "m1";
     await page.click('#user-name');
 }
 
